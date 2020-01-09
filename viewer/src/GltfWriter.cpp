300dfa5a0649ff1bdc68a158ff79e1eb09d49499
Catid <mrcatid@gmail.com>
2020-01-09 00:45:12 -0800

    WIP: JSON

--- viewer/src/GltfWriter.cpp
+++ viewer/src/GltfWriter.cpp
@@ -2,6 +2,7 @@
 
 #include "GltfWriter.hpp"
 
+#include <sstream>
 #include <fstream>
 
 namespace core {
@@ -26,19 +27,20 @@ namespace core {
             {
                 "buffer": 0,
                 "byteLength": 512,
-                "byteStride": 32,
+                "byteStride": 20,
                 "byteOffset": 0
             },
             {
                 "buffer": 0,
                 "byteLength": 512,
-                "byteStride": 32,
-                "byteOffset": 500
+                "byteStride": 20,
+                "byteOffset": 12
             },
             {
                 "buffer": 1,
                 "byteLength": 10242,
-                "byteOffset": 0
+                "byteStride": 4
+                "byteOffset": 0,
             }
         ],
         "images": [
@@ -55,16 +57,9 @@ namespace core {
                 "wrapT": 33071
             }
         ],
-        "materials": [
-            {
-                "name": "matt",
-                "doubleSided": false,
-                "pbrMetallicRoughness": {
-                    "baseColorFactor": [ 1.0, 1.0, 1.0, 1.0 ],
-                    "metallicFactor": 0.0,
-                    "roughnessFactor": 0.0
-                }
-            }
+        "textures" : [
+            "source": 0,
+            "sampler": 0
         ],
         "accessors": [
             {
@@ -76,7 +71,7 @@ namespace core {
             },
             {
                 "bufferView": 0,
-                "byteOffset": 12,
+                "byteOffset": 0,
                 "componentType": 5126,
                 "count": 2345,
                 "type": "VEC2"
@@ -89,18 +84,41 @@ namespace core {
                 "type": "SCALAR"
             }
         ],
+        "materials": [
+            {
+                "name": "matt",
+                "doubleSided": false,
+                "pbrMetallicRoughness": {
+                    "baseColorFactor": [ 1.0, 1.0, 1.0, 1.0 ],
+                    "metallicFactor": 0.0,
+                    "roughnessFactor": 0.0
+                }
+            }
+        ],
         "meshes": [
             {
                 "primitives": [
                     {
+                        "material": 0,
+                        "mode": 4,
                         "attributes": {
                             "POSITION": 0,
                             "TEXCOORD_0": 1
                         },
-                        "indices": 2,
-                        "material": 3,
-                        "mode": 4
+                        "indices": 2
                     }
+                ]
+            }
+        ],
+        "nodes": [
+            {
+                "name": "model1",
+                "mesh": 0,
+                "matrix": [
+                    1, 2, 3, 4,
+                    1, 2, 3, 4,
+                    1, 2, 3, 4,
+                    1, 2, 3, 4
                 ]
             }
         ],
@@ -115,37 +133,77 @@ namespace core {
                     "znear": 0.01
                 }      
             }
-        ],
-        "nodes": [
-            {
-                "name": "model1",
-                "mesh": 0,
-                "rotation": [
-                    0,
-                    0,
-                    0,
-                    1
-                ],
-                "translation": [
-                    -17.7082,
-                    -11.4156,
-                    2.0922
-                ]
-            }
         ]
     }
 */
-bool WriteFrameToGltfFile(const XrcapFrame& frame, const char* file_path)
+
+bool WriteFrameToGlbFile(const XrcapFrame& frame, const char* file_path)
 {
     std::ofstream file(file_path, std::ios::binary);
     if (!file) {
         return false;
     }
 
+    std::string chunkData1;
+    std::string chunkData2;
+
+    std::ostringstream json;
+    json << "{" << std::endl;
+    json << "    \"buffers\": [" << std::endl;
+    json << "        {" << std::endl;
+    json << "            \"byteLength\": " << chunkData1.size() << "," << std::endl;
+    json << "            \"uri\": \"model1.bin\"" << std::endl;
+    json << "        }," << std::endl;
+    json << "        {" << std::endl;
+    json << "            \"byteLength\": " << chunkData2.size() << "," << std::endl;
+    json << "            \"uri\": \"texture1.jpg\"" << std::endl;
+    json << "        }" << std::endl;
+    json << "    ]," << std::endl;
+    json << "    \"bufferViews\": [" << std::endl;
+    json << "        {" << std::endl;
+    json << "            \"buffer\": 0," << std::endl;
+    json << "            \"byteLength\": " << chunkData1.size() << "," << std::endl;
+    json << "            \"byteStride\": " << 20 << "," << std::endl;
+    json << "            \"byteOffset\": 0" << std::endl;
+    json << "        }," << std::endl;
+    json << "        {" << std::endl;
+                "buffer": 0,
+                "byteLength": 512,
+                "byteStride": 20,
+                "byteOffset": 12
+            },
+            {
+                "buffer": 1,
+                "byteLength": 10242,
+                "byteStride": 4
+                "byteOffset": 0,
+            }
+        ],
+        "images": [
+            {
+                "bufferView": 2,
+                "mimeType": "image/jpeg" 
+            }
+        ],
+
+    std::string chunkData0;
+
+    const uint32_t chunkLength0 = static_cast<uint32_t>( chunkData0.size() );
+    const uint32_t chunkLength1 = static_cast<uint32_t>( chunkData1.size() );
+    const uint32_t length = 12 + 8 + chunkLength0 + 8 + chunkLength1;
+
     const uint32_t version = 2;
     file.write("glTF", 4);
-    const uint32_t length = 2;
     file.write((const char*)&version, 4);
+    file.write((const char*)&length, 4);
+
+    file.write((const char*)&chunkLength0, 4);
+    file.write("JSON", 4);
+    file.write(chunkData0.c_str(), chunkData0.size());
+
+    file.write((const char*)&chunkLength1, 4);
+    file.write("BIN\0", 4);
+    file.write(chunkData1.c_str(), chunkData1.size());
 
     return true;
 }
